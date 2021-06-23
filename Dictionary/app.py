85eea5cb5d04dd101844d49edfc333d5a3fe8d27
roshanikhairnar <khairnar41997@gmail.com>
2021-06-23 12:08:59 +0530

    Add files via upload

--- Dictionary/app.py
+++ Dictionary/app.py
@@ -1,1 +1,28 @@
+import json
+from difflib import SequenceMatcher, get_close_matches
+data = json.load(open("data.json"))
 
+
+
+def returnDef(word,data):
+    word = word.lower()
+    if word in data: 
+        return data[word]
+    elif word.title() in data:     #for proper noun
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word,data.keys()))>0:
+        yn = input("did you mean %s ans Y/N? " % get_close_matches(word,data.keys())[0])
+        if yn == 'Y':
+            return data[get_close_matches(word,data.keys())[0]]
+        else:
+            return "word not exist"
+
+    else:
+        return "word not exist"
+
+
+word = input("enter word:")
+print(returnDef(word,data))   
+#len(get_close_matches(word,data.keys()))>0
